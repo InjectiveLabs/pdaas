b89e37619d41daf00bd47887f4a091c8a47a3136
ckhbtc <github@ckh.email>
2025-10-06 13:00:11 -0400

    refactor(ui): remove Standard tab header (bots disabled)

--- components/Partials/Trade/Futures/Form/Index.vue
+++ components/Partials/Trade/Futures/Form/Index.vue
@@ -22,18 +22,6 @@ onMounted(() => {
 
 <template>
   <div>
-    <div class="h-header flex border-b">
-      <AppButtonSelect
-        v-for="{ value, disabled } in options"
-        :key="value"
-        v-model="queryTradingMode"
-        v-bind="{ value, disabled }"
-        class="font-bold text-sm flex justify-center items-center px-6 border-r last:border-r-0 text-coolGray-450 flex-1"
-        active-classes="bg-brand-875 text-white"
-      >
-        {{ $t(`trade.${value}`) }}
-      </AppButtonSelect>
-    </div>
     <PartialsTradeFuturesFormStandard />
   </div>
 </template>
