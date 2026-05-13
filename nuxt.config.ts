2778be5c856176f4a8769963007af4ed73764a39
ckhbtc <github@ckh.email>
2026-05-13 14:38:20 -0400

    fix: pin remote injective ui layer

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,10 @@ import { head, hooks } from './nuxt-config'
 // Default to using the local vendored layer unless explicitly set to 'false'
 const isLocalLayer = process.env.LOCAL_LAYER !== 'false'
 const isProduction = process.env.NODE_ENV === 'production'
+const injectiveUiLayerRef =
+  process.env.NUXT_INJECTIVE_UI_LAYER_REF ||
+  'bdab2818f0b26f7371f14960ead6a4d39ce9399c'
+const remoteInjectiveUiLayer = `github:InjectiveLabs/injective-ui/layer#${injectiveUiLayerRef}`
 
 export default defineNuxtConfig({
   hooks,
@@ -70,9 +74,7 @@ export default defineNuxtConfig({
   },
 
   extends: [
-    isLocalLayer
-      ? './injective-ui'
-      : 'github:InjectiveLabs/injective-ui/layer#master'
+    isLocalLayer ? './injective-ui' : remoteInjectiveUiLayer
   ],
 
   // @ts-expect-error - typing issue
